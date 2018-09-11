2bcb0596551099cee539bfb47acf0c461721b1e2
DrugsZ <1002881508@qq.com>
2018-09-11 17:30:54 +0800

    修复

--- src/util/getLoc.js
+++ src/util/getLoc.js
@@ -12,24 +12,25 @@ let createLocWatch = () => {
 
 createLocWatch();
 
+
 export let getMapScript = (ak) => {
 	if(!global.AMap){
-		global.AMap = {}
-		const AMapScript  = new Promise( (resolve,reject) => {
+		global.AMap = {};
+		const AMapScript  = new Promise( (resolve) => {
 			global._initAMap = function(){
-				resolve(global.AMap)
+				resolve(global.AMap);
 				global.document.body.removeChild(mapScript);
 				global._initAMap = null;
-			}
+			};
 			const mapScript = document.createElement('script');
 			mapScript.src = `http://webapi.amap.com/maps?v=1.4.6&key=${ak}&callback=_initAMap`;
 			global.document.body.appendChild(mapScript);
-		})
+		});
 		return AMapScript;
 	}else{
-		return Promise.resolve(global.AMap)
+		return Promise.resolve(global.AMap);
 	}
-}
+};
 
 
 export let nowLoc = () => position;
