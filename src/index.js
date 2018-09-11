3575671a210bc4aeb6ebfce0bca9ada6107becce
DrugsZ <1002881508@qq.com>
2018-09-11 17:30:43 +0800

    增加热加载

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {AppContainer} from 'react-hot-loader';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (module.hot) {
+    module.hot.accept('./App', () => {
+      const NextApp = require('./App').default; // eslint-disable-line global-require
+      ReactDOM.render(
+        <App>
+          <NextApp />
+        </App>,
+        document.getElementById('root')
+      );
+  });
+}
+
+ReactDOM.render(
+  <AppContainer>
+    <App />
+  </AppContainer>, 
+  document.getElementById('root'));
 registerServiceWorker();
