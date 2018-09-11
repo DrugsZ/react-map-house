5ca7462cc6d9a43c67d414ab777807d2af938f98
DrugsZ <1002881508@qq.com>
2018-09-11 11:47:37 +0800

    添加获取地理位置,异步加载高德地图SDK

---
+++ src/util/getLoc.js
@@ -0,0 +1,37 @@
+let position = {};
+
+let showPosition = (result) => {
+	position = result;
+};
+
+let createLocWatch = () => {
+	if (navigator.geolocation) {
+		navigator.geolocation.watchPosition(showPosition);
+	}
+};
+
+export let getMapScript = (ak) => {
+	if(!global.AMap){
+		global.AMap = {}
+		const AMapScript  = new Promise( (resolve,reject) => {
+			global._initAMap = function(){
+				resolve(global.AMap)
+				global.document.body.removeChild(mapScript);
+				global._initAMap = null;
+			}
+			const mapScript = document.createElement('script');
+			mapScript.src = `http://webapi.amap.com/maps?v=1.4.6&key=${ak}&callback=_initAMap`;
+			global.document.body.appendChild(mapScript);
+		})
+		return AMapScript;
+	}else{
+		return Promise.resolve(global.AMap)
+	}
+}
+
+createLocWatch();
+
+export let nowLoc = () => position;
+// export default {
+// 	getLoc
+// }
