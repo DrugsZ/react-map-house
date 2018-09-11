575193385bbc9e4f1b786210b9d767bd493133b8
DrugsZ <1002881508@qq.com>
2018-09-11 17:31:16 +0800

    删除测试代码

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import AMap from './map'
-import { nowLoc } from "./util/getLoc";
+import AMap from './map';
+import { nowLoc } from './util/getLoc';
 
-if(module.hot) {
-  module.hot.accept('./App',() => {
-      render(App);
-  });
-}
 
 class App extends Component {
-	componentDidMount(){
-		window.nowLoc = nowLoc;
-		nowLoc();
-	}
-	render() {
-		return (
-			<div className="App">
-				<AMap style={{'height':'100vh'}}></AMap>
-			</div>
-		);
-	}
+  componentDidMount(){
+    window.nowLoc = nowLoc;
+    nowLoc();
+  }
+  render() {
+    return (
+      <div className="App">
+        <AMap style={{'height':'100vh'}}></AMap>
+      </div>
+    );
+  }
 }
 
 export default App;
